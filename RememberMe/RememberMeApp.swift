927c2e1d7f5ca2fa9b97223b22bfcfc5bf528412
IndyWins <179518152+IndyWins@users.noreply.github.com>
2025-05-16 08:22:26 +0100

    Initial Commit

---
+++ RememberMe/RememberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberMeApp.swift
+//  RememberMe
+//
+//  Created by Richard Clarke on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RememberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
